4e76ff3ed92e7acee48ed6d09c71b9c5aa4b6172
Jithendra-ayesh <jithendraayesh25@gmail.com>
2026-06-09 12:31:32 +0530

    Add login page and authentication validation

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<h1>Mini SIEM Dashboard</h1>"
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "admin" and password == "1234":
+            return "<h2>Login Successful</h2>"
+
+        return "<h2>Login Failed</h2>"
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
