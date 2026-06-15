de586d5e71d31a637669a4e279ceeb5ed3baad14
Jithendra-ayesh <jithendraayesh25@gmail.com>
2026-06-15 11:33:31 +0530

    Add brute force attack simulation script

---
+++ simulations/brute_force_simulator.py
@@ -0,0 +1,19 @@
+import requests
+
+TARGET_URL = "http://127.0.0.1:5000/login"
+
+for i in range(10):
+
+    data = {
+        "username": "admin",
+        "password": f"wrong{i}"
+    }
+
+    requests.post(
+        TARGET_URL,
+        data=data
+    )
+
+    print(
+        f"Attempt {i+1} sent"
+    )
